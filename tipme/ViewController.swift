380dcf26413a3f5d69dd73ef24736a3052ce2b15
Christian Lapinig <clapinig93@gmail.com>
2019-12-18 23:40:18 -0800

    Initial Commit

---
+++ tipme/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tipme
+//
+//  Created by Christian Lapinig on 12/18/19.
+//  Copyright © 2019 Christian Lapinig. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
